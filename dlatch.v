eb8af18b7c7b781715ad61af609fd4b83899aeb3
kdharani242-source <kdharani242@gmail.com>
2026-01-14 11:08:17 +0530

    Create dlatch.v

---
+++ dlatch.v
@@ -0,0 +1,28 @@
+// Code your design here
+module dlatch(en,d,q);
+  input en,d;
+  output q;
+  assign q=en?d:q;
+endmodule
+
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg en,q;
+  wire q;
+  dlatch u1(en,d,q);
+  initial
+    begin
+      $monitor("en=%b d=%b q=%b",en,d,q);
+      for(int i=0;i<4;i++) begin
+        {en,d}=i;#1;
+      end
+    end
+endmodule
+//output
+# KERNEL: en=0 d=0 q=x
+# KERNEL: en=0 d=1 q=x
+# KERNEL: en=1 d=0 q=0
+# KERNEL: en=1 d=1 q=1
+# KERNEL: en=0 d=0 q=1
+# KERNEL: en=1 d=0 q=0
